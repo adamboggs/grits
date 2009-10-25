e4fa86cae855fda1a303c74ce6f4c6a7cad99fc2
Andy Spencer <andy753421@gmail.com>
2009-10-25 20:36:46 +0000

    Move in-place includes

--- gen.sh
+++ gen.sh
@@ -3,6 +3,6 @@ dir=$(dirname $(readlink -f $0))
 ./autogen.sh \
 	"--enable-gtk-doc" \
 	"--libdir=$dir/src/.libs" \
-	"--includedir=$dir/src" \
+	"--includedir=$dir/include" \
 	CFLAGS="-g -Werror -Wno-unused $CFLAGS" \
 	LDFLAGS="-Wl,-z,defs"
