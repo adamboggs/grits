08b979cf4e173ae461ce0e823ba08a732f4b7cbf
Andy Spencer <andy753421@gmail.com>
2010-05-02 01:31:59 +0000

    Win32 build fixes

--- gen-win32.sh
+++ gen-win32.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-dir=$(dirname $(readlink -f $0))
-./autogen.sh \
-	"--host=i686-pc-mingw32" \
-	"--libdir=Z:$dir/src/.libs" \
-	"--includedir=Z:$dir/include" \
-	CFLAGS="-g -Werror -Wno-unused $CFLAGS"
+export CFLAGS="-g -Werror -Wno-unused -O3"
+./autogen.sh --host=i686-pc-mingw32
