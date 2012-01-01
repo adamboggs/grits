52a1cbcfe7898d16be07813cacc8ae97515ba621
Andy Spencer <andy753421@gmail.com>
2012-01-01 13:01:27 -0800

    Move build scripts to separate repository

--- gen-win32.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./autogen.sh \
-	--enable-shared \
-	--disable-static \
-	--host=i686-pc-mingw32 \
-	CFLAGS="-g -Werror -Wno-unused -O3"
--- gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dir=$(dirname $(readlink -f $0))
-./autogen.sh \
-	"--enable-gtk-doc" \
-	"--libdir=$dir/src/.libs" \
-	"--includedir=$dir/include" \
-	CFLAGS="-g -Werror $CFLAGS"
